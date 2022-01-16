e2530b73b49d235a8f21c47622c263028a52cc97
Louis Maddox <louismmx@gmail.com>
2022-01-16 15:15:07 +0000

    Load first book, nice repr

--- src/dex/isbn_utils.py
+++ src/dex/isbn_utils.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
+from dataclasses import dataclass
+from enum import Enum, auto
 
 from isbnlib import Isbn, meta
-from isbnlib._exc import NotValidISBNError
-from dataclasses import dataclass
+from isbnlib._exceptions import NotValidISBNError
 
+__all__ = ["validate_isbn", "get_isbn_metadata", "BookMetadata"]
 
 def validate_isbn(isbn_code: str) -> bool:
     try:
@@ -14,19 +16,59 @@ def validate_isbn(isbn_code: str) -> bool:
         is_isbn = True
     return is_isbn
 
+class TitledEnum(Enum):
+    def _generate_next_value_(name, start, count, last_values):
+        return name.title()
+
+    @classmethod
+    def names2values(cls) -> list[str]:
+        return {m.name: v for v,m in cls._value2member_map_.items()}
+
+class MetaEnum(TitledEnum):
+    """
+    Internal representations of keys from the :class:`isbnlib.Isbn` class.
+    """
+    title = auto()
+    authors = auto()
+    year = auto()
+    isbn_13 = "ISBN-13"
+    publisher = auto()
 
 def get_isbn_metadata(isbn_code: str) -> dict[str, str | list[str]]:
     book_metadata = meta(isbn_code)
-    meta_keys = "Title Authors Year ISBN-13".split()
-    title, authors, year, isbn_13 = map(book_metadata.get, meta_keys)
-    meta_cls = BookMetadata(title=title, authors=authors, year=year, isbn_13=isbn_13)
+    #title, authors, year, isbn_13, publisher = map(book_metadata.get, MetaEnum.values())
+    book_meta_kwargs = {
+        name: book_metadata.get(value)
+        for name, value in MetaEnum.names2values().items()
+    }
+    meta_cls = BookMetadata(**book_meta_kwargs)
     return meta_cls
 
 
+class IndexedBook:
+    @property
+    def first_author(self):
+        return self.authors[0]
+
+    @property
+    def first_author_surname(self) -> str:
+        return self.author_surname(self.first_author)
+
+    @staticmethod
+    def author_surname(name: str) -> str:
+        return name.split(" ")[-1]
+
+    @property
+    def short_fmt(self) -> str:
+        return f"{self.first_author_surname} ({self.year}) {self.title}"
+
 @dataclass
-class BookMetadata:
+class BookMetadata(IndexedBook):
     title: str
     authors: list[str]
     year: int
     publisher: str
     isbn_13: str
+
+    def __repr__(self) -> str:
+        return f"'\N{open book}: {self.short_fmt}'"
--- src/dex/library.py
+++ src/dex/library.py
@@ -1,6 +1,13 @@
 from __future__ import annotations
 
+import re
+from dataclasses import dataclass
+from functools import partial
+from pathlib import Path
+
+from .isbn_utils import BookMetadata, get_isbn_metadata
 from .log_utils import Console
+from .multiproc_utils import batch_multiprocess_with_return
 from .path_utils import shelves_path
 
 __all__ = ["load_library"]
@@ -8,11 +15,58 @@ __all__ = ["load_library"]
 logger = Console(name=__name__).logger
 
 
-def load_library() -> None:
+def load_library() -> Library:
     pattern = "dex*.zip"
     zipfiles = [*shelves_path.glob(pattern)]
-    if zipfiles:
-        logger.info(f"Library contains {len(zipfiles)} files")
-    else:
-        logger.info("Nothing to do")
-    return
+    lib = Library.from_zips(zipfiles)
+    return lib
+
+
+class Library(list):
+    def __init__(self, library_items: list[LibraryItem]):
+        if not library_items:
+            logger.info("Library is empty")
+        else:
+            logger.info(f"Library contains {len(library_items)} files")
+        self.append(library_items)
+
+    @classmethod
+    def from_zips(cls, zipfiles: list[Path], parallel: bool = False):
+        if parallel:
+            zip_items = batch_multiprocess_with_return(
+                function_list=[partial(LibraryItem.from_zip, zf) for zf in zipfiles]
+            )
+        else:
+            zip_items = [LibraryItem.from_zip(zf) for zf in zipfiles]
+        return cls(library_items=zip_items)
+
+
+class IndexedItem:
+    isbn_regex = r"[0-9]{10,13}"
+
+    @property
+    def is_archived(self) -> bool:
+        return self.archive is not None
+
+    @classmethod
+    def from_isbn(cls, isbn_code: str, archive_path: Path | None = None) -> LibraryItem:
+        metadata = get_isbn_metadata(isbn_code)
+        return cls(metadata=metadata, archive_path=archive_path)
+
+    @classmethod
+    def from_zip(cls, zipfile: Path) -> LibraryItem:
+        suffixes = [".zip"]
+        if zipfile.suffix not in suffixes:
+            raise ValueError(f"{zipfile} not recognised as a ZIP file")
+        try:
+            isbn_code = next(re.finditer(cls.isbn_regex, zipfile.stem)).group()
+        except StopIteration:
+            logger.error(f"Could not detect ISBN in filename {zipfile.stem} (omitting)")
+        else:
+            return cls.from_isbn(isbn_code=isbn_code, archive_path=zipfile)
+
+
+@dataclass
+class LibraryItem(IndexedItem):
+    metadata: BookMetadata
+    archive_path: Path | None
