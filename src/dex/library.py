9ed0b463edd0ed041a136c13837c3f05cddd515d
Louis Maddox <louismmx@gmail.com>
2024-08-18 22:42:37 +0100

    refactor: remove multiple return sites (use an iterator)

--- src/dex/library.py
+++ src/dex/library.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from pathlib import Path
+from typing import Iterator
 
 from pydantic import ValidationError
 
@@ -8,7 +9,7 @@ from .data_model import Book, Library, Shelving
 from .isbn_utils import get_isbn_metadata, isbn_ta
 from .log_utils import Console
 
-__all__ = ["load_library"]
+__all__ = ["load_library", "take_books_from_shelves"]
 
 logger = Console(name=__name__).logger
 
@@ -17,17 +18,17 @@ def load_library(n: int | None = None) -> Library:
     """Pass ``n`` to just load that number of shelves (used for fast dev iteration)."""
     dirs = Shelving().shelves[:n]
     # Filter non-null results from calling `Book.from_shelf` on all `shelf_dirs`
-    return Library(items=filter(None, map(take_book_from_shelf, dirs)))
+    return Library(items=take_books_from_shelves(dirs))
 
 
-def take_book_from_shelf(shelf_dir: Path) -> Book | None:
+def take_books_from_shelves(shelves: list[Path]) -> Iterator[Book]:
     """TODO: this should not be a class method, make a `shelves` module."""
-    dir_name = shelf_dir.stem
-    try:
-        isbn_code = isbn_ta.validate_strings(dir_name)
-    except ValidationError:
-        logger.warning(f"Could not detect ISBN in {dir_name=} (omitting)")
-        return None
-    else:
-        metadata = get_isbn_metadata(isbn_code)
-        return Book(metadata=metadata, shelf=shelf_dir)
+    for shelf_dir in shelves:
+        dir_name = shelf_dir.stem
+        try:
+            isbn_code = isbn_ta.validate_strings(dir_name)
+        except ValidationError:
+            logger.warning(f"Could not detect ISBN in {dir_name=} (omitting)")
+        else:
+            metadata = get_isbn_metadata(isbn_code)
+            yield Book(metadata=metadata, shelf=shelf_dir)
